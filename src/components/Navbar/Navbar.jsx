5db7d04febb31a42bc4c1f4e07f4db1b60bac1a8
ahmed-deftoner <70218377+ahmed-deftoner@users.noreply.github.com>
2022-02-02 18:31:23 +0500

    navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {GiHamburgerMenu} from 'react-icons/gi';
+import {MdOutlineRestaurantMenu} from 'react-icons/md';
+
+import images from '../../constants/images';
+
+import './Navbar.css';
+
+const Navbar = () => (
+  <nav className="app__navbar">
+    <div className="app__navbar-logo">
+       <img src={images.gericht} alt="app logo"/>
+    </div>
+    <ul className="app__navbar-links">
+      <li className="p-opensans" style={{color: 'white'}}><a href="#home">Home</a></li>
+      <li className="p-opensans" style={{color: 'white'}}><a href="#about">About</a></li>
+      <li className="p-opensans" style={{color: 'white'}}><a href="#menu">Menu</a></li>
+      <li className="p-opensans" style={{color: 'white'}}><a href="#awards">Awards</a></li>
+      <li className="p-opensans" style={{color: 'white'}}><a href="#contact">Contact</a></li>
+    </ul>
+    <div className="app__navbar-login">
+       <a href="#login" className="p__opensans">Log In / Registration</a>
+        <div />
+        <a href="/" className="p__opensans">Book Table</a>
+    </div>
+    <div className="app__navbar-smallscreen">
+      <GiHamburgerMenu fontSize={27} color='#fff' onClick={() => {}} />
+      <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => {}} />
+            <ul className="app__navbar-smallscreen_links">
+            <li className="p-opensans" style={{color: 'white'}}><a href="#home">Home</a></li>
+            <li className="p-opensans" style={{color: 'white'}}><a href="#about">About</a></li>
+            <li className="p-opensans" style={{color: 'white'}}><a href="#menu">Menu</a></li>
+            <li className="p-opensans" style={{color: 'white'}}><a href="#awards">Awards</a></li>
+            <li className="p-opensans" style={{color: 'white'}}><a href="#contact">Contact</a></li>
+            </ul>
+      </div>
+    </div>
+  </nav>
+);
+
+export default Navbar;
